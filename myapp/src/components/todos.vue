aaaf82d7739a282a95b680e083949ab95b3d8f1e
Allan <allan.vixel@etudiant.univ-rennes1.fr>
2020-11-13 17:11:44 +0100

    Fin tp

---
+++ myapp/src/components/todos.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="todos">
+    <h1>TODOS</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'todos',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .todos {
+
+  }
+</style>
---
+++ myapp/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <p v-if="visible">Maintenant vous me voyez {{message}}</p>
+    <button v-on:click="foo()">
+      ok
+    </button>
+     <div v-background>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
+    <div v-background="green">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
+    <Todos />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Todos from '@/components/todos.vue';
+import { Background } from '@/directives/background.directive';
+@Component({
+  components: {
+    Todos,
+  },
+  directives:{
+    Background
+  }
+})
+export default class Home extends Vue {
+
+    message= 'test';
+    visible = true;
+    foo(){
+      this.visible = !this.visible;
+    }
+}
+</script>
